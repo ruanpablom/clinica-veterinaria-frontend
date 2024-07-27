2d86ad755e748ecaf2bedc7650da6766208da7bb
Ruan Medeiros <pm.ruan@gmail.com>
2024-07-27 19:28:14 -0300

    refactor: adicionar icone ao titulo da página de pets

--- src/pages/Pets/index.tsx
+++ src/pages/Pets/index.tsx
@@ -1,7 +1,13 @@
+import { MdOutlinePets } from "react-icons/md"
+
 export function Pets() {
   return (
     <div>
-      <h1>Pets</h1>
+        <div className="page-title">
+            <MdOutlinePets id="icon" size={36} />
+            <h1>Pets</h1>
+        </div>
+        
     </div>
   );
 }
