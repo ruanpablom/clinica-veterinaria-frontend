eb2c2c4d6a5c1e9156f3d88755e8daa7b02fd6dd
Ruan Medeiros <pm.ruan@gmail.com>
2024-07-27 20:13:58 -0300

    refactor: adicionar container de ações página de pet

--- src/pages/Pets/index.tsx
+++ src/pages/Pets/index.tsx
@@ -1,13 +1,20 @@
 import { MdOutlinePets } from "react-icons/md"
+import './styles.css'
 
 export function Pets() {
   return (
-    <div>
+    <div className="container">
         <div className="page-title">
             <MdOutlinePets id="icon" size={36} />
             <h1>Pets</h1>
         </div>
-        
+        <div className="acoes-container">
+            <button id="btn-cadastrar">Cadastrar</button>
+            <div className="pesquisa-container">
+                <input type="text" placeholder="Pesquisar" />
+                <button>Pesquisar</button>
+            </div>
+        </div>
     </div>
   );
 }
